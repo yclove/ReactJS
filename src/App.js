42f9b74d27517f95e5ec0aeec78e8badb2ba1a9f
yclove7 <yclove7@gmail.com>
2024-06-14 00:37:30 +0900

    버전업으로 인한 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>YCLOVE</h1>
+      <h1 className={styles.title}>YCLOVE</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
