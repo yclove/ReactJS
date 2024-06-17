c8884d5c8f4d319d834ad5614888504d04b1305c
yclove7 <yclove7@gmail.com>
2024-06-17 10:42:43 +0900

    Github Route 에 맞게 수정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
-  return <Router>
-    <Routes>
-      <Route path="/hello" element={
-        <h1>Hello</h1>
-      } />
-      <Route path="/" element={<Home />} />
-      <Route path="/movie/:id" element={<Detail />} />
-    </Routes>
-  </Router>
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
