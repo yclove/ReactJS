53a865be966c6b2ea0548ffd90f36c14f07f638d
yclove7 <yclove7@gmail.com>
2024-06-18 15:19:06 +0900

    주석추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+/**
+ * 브라우저는 HTML, CSS, JavaScript만 읽을 수 있기 때문에 우리가 작성한 React를 읽지 못한다.
+ * 따라서 React로 작성한 코드를 브라우저가 읽을 수 있도록 변환해주어야한다.
+ * 리액트에서 JSX 문법을 사용하는데, 이 JSX 문법을 자바스크립트로 변환시킬 때 JSX transformer를 사용해 JSX로 작성된 React 메소드를 변환시킨다.
+ * 이때 객체인 React를 가져오기 위해서 import React from ‘react’를 맨 위에 작성해 React를 불러오게 되면 객체 React를 통해 React 메소드를 작성할 수 있게 된다.
+ *
+ * React 버전 17부터 생략 가능!
+ * 2020년 10월 20일에 릴리즈된 React v17를 보면,
+ * React 내부적으로 JSX transformer가 JSX를 React 요소로 변환하는 작업을 거치기 때문에 모든 컴포넌트에 import React from ‘react’를 작성하지 않아도 된다!
+ */
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,43 +1,99 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import { useParams } from "react-router-dom";
 
+const useConfirm = (message = "", callback, rejection) => {
+  if (typeof callback !== "function" || typeof rejection !== "function") {
+    return;
+  }
+
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
+};
+
+const useClick = (onClick) => {
+  //   if (typeof onClick !== "function") {
+  //     return;
+  //   }
+  /**
+   * Issue : React Hook "useRef / useEffect" is called conditionally. React Hooks must be called in the exact same order in every component render.
+   * React Hook의 2원칙
+   * 1. Hook 은 최상위에서만 선언되어야 한다.
+   *    - 반복문, 조건문 혹은 중첩된 함수 내에서 훅을 호출하면 안된다.
+   *    - 리액트는 Hook이 호출되는 순서에 의존하기 때문에 Hook의 실행순서가 밀리게 되거나 건너뛰면 버그를 야기한다.
+   *    - 만약 조건부로 Hook을 쓰고 싶다면 Hook 내부에 선언해야 한다.
+   * 2. 오직 React 함수 내에서만 Hook을 호출해야 한다.
+   *    - 훅을 일반적인 자바스크립트 함수에서 호출하면 안된다.
+   *    - 대신 리액트 함수 컴포넌트나 커스텀 훅에서는 호출이 가능하다.
+   */
+  const element = useRef();
+
+  useEffect(() => {
+    // [ if ] componentDidMount 시 호출
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    // [ return ] componentWillUnMount 시 호출
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
 function Detail() {
-    const { id } = useParams();
-    const fetchUrl = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
-    const [loading, setLoading] = useState(true);
-    const [movie, setMovie] = useState({});
-    /**
-     * useCallback 은 우리가 지난 시간에 배웠던 useMemo 와 비슷한 Hook 입니다.
-     * useMemo 는 특정 결과값을 재사용 할 때 사용하는 반면,
-     * useCallback 은 특정 함수를 새로 만들지 않고 재사용하고 싶을때 사용합니다.
-     */
-    const getMovie = useCallback(async () => {
-        const json = await (
-            await fetch(fetchUrl)
-        ).json();
-        setMovie(json.data.movie);
-        setLoading(false);
-    }, [fetchUrl]);
-    useEffect(() => {
-        getMovie();
-        // Issue : React Hook useEffect has a missing dependency: 'getMovie'. Either include it or remove the dependency array
-        // 기능적으로 block해서 일시적으로 해결하는 방법
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  const sayHello = () => console.log("Say Hello");
+  const title = useClick(sayHello);
 
-    return (
+  const { id } = useParams();
+  const fetchUrl = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
+  /**
+   * useCallback 은 우리가 지난 시간에 배웠던 useMemo 와 비슷한 Hook 입니다.
+   * useMemo 는 특정 결과값을 재사용 할 때 사용하는 반면,
+   * useCallback 은 특정 함수를 새로 만들지 않고 재사용하고 싶을때 사용합니다.
+   */
+  const getMovie = useCallback(async () => {
+    const json = await (await fetch(fetchUrl)).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  }, [fetchUrl]);
+  useEffect(() => {
+    getMovie();
+    // Issue : React Hook useEffect has a missing dependency: 'getMovie'. Either include it or remove the dependency array
+    // 기능적으로 block해서 일시적으로 해결하는 방법
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onDelete = () => {
+    console.log("Delete");
+  };
+  const onAbort = () => {
+    console.log("Abort");
+  };
+  const confirmDelete = useConfirm("Are you sure?", onDelete, onAbort);
+
+  return (
+    <div>
+      <h1>Detail</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
         <div>
-            <h1>Detail</h1>
-            {loading ? (
-                <strong>Loading...</strong>
-            ) : (
-                <div>
-                    <h1>{movie.title}</h1>
-
-                </div>
-            )}
+          <h1 ref={title}>{movie.title}</h1>
+          <button onClick={confirmDelete}>Delete Movie</button>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Detail;
